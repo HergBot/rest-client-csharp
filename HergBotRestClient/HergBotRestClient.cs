85ac4a45f68ac7d536eabba1cdc645bca3401af6
Justin <xhergz@gmail.com>
2019-08-18 08:22:30 -0400

    Added in the main class for the HergBotRestClient that will be the most used way to access the library externally.

---
+++ HergBotRestClient/HergBotRestClient.cs
@@ -0,0 +1,56 @@
+﻿using HergBot.RestClient.Http;
+
+namespace HergBot.RestClient
+{
+    public class HergBotRestClient
+    {
+        private IHttpClient _client;
+
+        public HergBotRestClient(string authToken)
+        {
+            InitializeHttpClient(authToken);
+        }
+
+        public HttpResponse Get(string apiUrl)
+        {
+            HttpRequest getRequest = new HttpRequest(_client, apiUrl);
+            return getRequest.Send(HttpVerb.GET).Result;
+        }
+
+        public HttpResponse Get(string apiUrl, UniqueIdParameter uidParam)
+        {
+            HttpRequest getRequest = new HttpRequest(_client, apiUrl);
+            return getRequest.Send(HttpVerb.GET, uidParam).Result;
+        }
+
+        public HttpResponse Get(string apiUrl, QueryParameter queryParam)
+        {
+            HttpRequest getRequest = new HttpRequest(_client, apiUrl);
+            return getRequest.Send(HttpVerb.GET, queryParam).Result;
+        }
+
+        public HttpResponse Post(string apiUrl, UniqueIdParameter uidParam, JsonBodyParameter body)
+        {
+            HttpRequest getRequest = new HttpRequest(_client, apiUrl);
+            return getRequest.Send(HttpVerb.POST, uidParam, body).Result;
+        }
+
+        public HttpResponse Put(string apiUrl, JsonBodyParameter body)
+        {
+            HttpRequest getRequest = new HttpRequest(_client, apiUrl);
+            return getRequest.Send(HttpVerb.PUT, null, body).Result;
+        }
+
+        public HttpResponse Delete(string apiUrl, UniqueIdParameter uidParam)
+        {
+            HttpRequest getRequest = new HttpRequest(_client, apiUrl);
+            return getRequest.Send(HttpVerb.DELETE, uidParam).Result;
+        }
+
+        private void InitializeHttpClient(string authToken)
+        {
+            _client = new HttpClientHandler();
+            _client.SetBearerToken(authToken);
+        }
+    }
+}
