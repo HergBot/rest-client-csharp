a5b9a5b744c3029743356dbf634ef0ed6d009640
Justin <xhergz@gmail.com>
2020-01-14 13:01:42 -0500

    Modified the HergBotRestClient so the bearer token is set every time a method will be call due to it being overwritten by subsequent instances.

--- HergBotRestClient/HergBotRestClient.cs
+++ HergBotRestClient/HergBotRestClient.cs
@@ -6,51 +6,60 @@ namespace HergBot.RestClient
     {
         private IHttpClient _client;
 
+        private string _authToken;
+
         public HergBotRestClient(string authToken)
         {
-            InitializeHttpClient(authToken);
+            InitializeHttpClient();
         }
 
         public HttpResponse Get(string apiUrl)
         {
             HttpRequest getRequest = new HttpRequest(_client, apiUrl);
+            // The bearer token must be set every time because the client is static behind the scenes and will
+            // mean tokens are overwritten by subsequent instances of this class or other IRestClient interfaces.
+            _client.SetBearerToken(_authToken);
             return getRequest.Send(HttpVerb.GET).Result;
         }
 
         public HttpResponse Get(string apiUrl, UniqueIdParameter uidParam)
         {
             HttpRequest getRequest = new HttpRequest(_client, apiUrl);
+            _client.SetBearerToken(_authToken);
             return getRequest.Send(HttpVerb.GET, uidParam).Result;
         }
 
         public HttpResponse Get(string apiUrl, QueryParameter queryParam)
         {
             HttpRequest getRequest = new HttpRequest(_client, apiUrl);
+            _client.SetBearerToken(_authToken);
             return getRequest.Send(HttpVerb.GET, queryParam).Result;
         }
 
         public HttpResponse Post(string apiUrl, UniqueIdParameter uidParam, JsonBodyParameter body)
         {
             HttpRequest getRequest = new HttpRequest(_client, apiUrl);
+            _client.SetBearerToken(_authToken);
             return getRequest.Send(HttpVerb.POST, uidParam, body).Result;
         }
 
         public HttpResponse Put(string apiUrl, JsonBodyParameter body)
         {
             HttpRequest getRequest = new HttpRequest(_client, apiUrl);
+            _client.SetBearerToken(_authToken);
             return getRequest.Send(HttpVerb.PUT, null, body).Result;
         }
 
         public HttpResponse Delete(string apiUrl, UniqueIdParameter uidParam)
         {
             HttpRequest getRequest = new HttpRequest(_client, apiUrl);
+            _client.SetBearerToken(_authToken);
             return getRequest.Send(HttpVerb.DELETE, uidParam).Result;
         }
 
-        private void InitializeHttpClient(string authToken)
+        private void InitializeHttpClient()
         {
             _client = new HttpClientHandler();
-            _client.SetBearerToken(authToken);
         }
     }
 }
