3486e4b55345104370c0db3e4143f5908583a148
Justin <xhergz@gmail.com>
2020-01-17 13:15:36 -0500

    Documented the HergBotRestClient, HttpClientHandler, and DataParameter classes.

--- HergBotRestClient/HergBotRestClient.cs
+++ HergBotRestClient/HergBotRestClient.cs
@@ -2,17 +2,36 @@
 
 namespace HergBot.RestClient
 {
+    /// <summary>
+    /// A class to wrap the functionality to communicate with a REST API or make web requests.
+    /// </summary>
     public class HergBotRestClient : IRestClient
     {
+        /// <summary>
+        /// The HTTP Client interface to use under the hood.
+        /// </summary>
         private IHttpClient _client;
 
+        /// <summary>
+        /// The bearer token to be sent in the Authorization header.
+        /// </summary>
         private string _authToken;
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="authToken">The bearer token to be used for authorization.</param>
         public HergBotRestClient(string authToken)
         {
-            InitializeHttpClient();
+            _authToken = authToken;
+            _client = new HttpClientHandler();
         }
 
+        /// <summary>
+        /// Performs a plain GET request.
+        /// </summary>
+        /// <param name="apiUrl">The URL to perform the request on.</param>
+        /// <returns>The HTTP Response information.</returns>
         public HttpResponse Get(string apiUrl)
         {
             HttpRequest getRequest = new HttpRequest(_client, apiUrl);
@@ -22,6 +41,12 @@ namespace HergBot.RestClient
             return getRequest.Send(HttpVerb.GET).Result;
         }
 
+        /// <summary>
+        /// Performs a GET request with a unique ID (i.e. www.url.com/items/1).
+        /// </summary>
+        /// <param name="apiUrl">The URL to perform the request on.</param>
+        /// <param name="uidParam">The Unique ID (i.e. the thing after the /).</param>
+        /// <returns>The HTTP Response information.</returns>
         public HttpResponse Get(string apiUrl, UniqueIdParameter uidParam)
         {
             HttpRequest getRequest = new HttpRequest(_client, apiUrl);
@@ -29,6 +54,12 @@ namespace HergBot.RestClient
             return getRequest.Send(HttpVerb.GET, uidParam).Result;
         }
 
+        /// <summary>
+        /// Performs a GET request with query parameters (i.e. www.url.com/items?some=value&other=value).
+        /// </summary>
+        /// <param name="apiUrl">The URL to perform the request on.</param>
+        /// <param name="queryParam">The query parameters.</param>
+        /// <returns>The HTTP Response information.</returns>
         public HttpResponse Get(string apiUrl, QueryParameter queryParam)
         {
             HttpRequest getRequest = new HttpRequest(_client, apiUrl);
@@ -36,6 +67,13 @@ namespace HergBot.RestClient
             return getRequest.Send(HttpVerb.GET, queryParam).Result;
         }
 
+        /// <summary>
+        /// Performs a POST request.
+        /// </summary>
+        /// <param name="apiUrl">The URL to perform the request on.</param>
+        /// <param name="uidParam">The Unique ID (i.e. the thing after the /).</param>
+        /// <param name="body">A JSON body with additional details.</param>
+        /// <returns>The HTTP Response information.</returns>
         public HttpResponse Post(string apiUrl, UniqueIdParameter uidParam, JsonBodyParameter body)
         {
             HttpRequest getRequest = new HttpRequest(_client, apiUrl);
@@ -43,6 +81,12 @@ namespace HergBot.RestClient
             return getRequest.Send(HttpVerb.POST, uidParam, body).Result;
         }
 
+        /// <summary>
+        /// Performs a PUT request.
+        /// </summary>
+        /// <param name="apiUrl">The URL to perform the request on.</param>
+        /// <param name="body">A JSON body with additional details.</param>
+        /// <returns>The HTTP Response information.</returns>
         public HttpResponse Put(string apiUrl, JsonBodyParameter body)
         {
             HttpRequest getRequest = new HttpRequest(_client, apiUrl);
@@ -50,16 +94,17 @@ namespace HergBot.RestClient
             return getRequest.Send(HttpVerb.PUT, null, body).Result;
         }
 
+        /// <summary>
+        /// Performs a DELETE request.
+        /// </summary>
+        /// <param name="apiUrl">The URL to perform the request on.</param>
+        /// <param name="uidParam">The Unique ID (i.e. the thing after the /).</param>
+        /// <returns>The HTTP Response information.</returns>
         public HttpResponse Delete(string apiUrl, UniqueIdParameter uidParam)
         {
             HttpRequest getRequest = new HttpRequest(_client, apiUrl);
             _client.SetBearerToken(_authToken);
             return getRequest.Send(HttpVerb.DELETE, uidParam).Result;
-        }
-
-        private void InitializeHttpClient()
-        {
-            _client = new HttpClientHandler();
         }
     }
 }
--- HergBotRestClient/Http/HttpClientHandler.cs
+++ HergBotRestClient/Http/HttpClientHandler.cs
@@ -4,35 +4,82 @@ using System.Threading.Tasks;
 
 namespace HergBot.RestClient.Http
 {
+    /// <summary>
+    /// A class to interface with .NET's HTTP client for making web requests.
+    /// </summary>
     public class HttpClientHandler : IHttpClient
     {
+        /// <summary>
+        /// The HTTP client object. It is reccomended by Microsoft to use only 1 static instance of this class.
+        /// See: https://docs.microsoft.com/en-us/dotnet/api/system.net.http.httpclient?view=netframework-4.8
+        /// </summary>
         private static readonly HttpClient _client = new HttpClient();
 
+        /// <summary>
+        /// Wraps the DeleteAsync method.
+        /// </summary>
+        /// <param name="url">The URL to send the request to.</param>
+        /// <returns>The HTTP response information.</returns>
+        /// <exception cref="System.ArgumentNullException">Thrown when the URL is null.</exception>
+        /// <exception cref="System.InvalidOperationException">Thrown when the request was already sent by 
+        /// the client instance.</exception>
+        /// <exception cref="HttpRequestException">Thrown when the request fails due to an underlying issue.</exception>
         public async Task<HttpResponseMessage> DeleteAsync(string url)
         {
             return await _client.DeleteAsync(url);
         }
 
+        /// <summary>
+        /// Wraps the GetAsync method.
+        /// </summary>
+        /// <param name="url">The URL to send the request to.</param>
+        /// <returns>The HTTP response information.</returns>
+        /// <exception cref="System.ArgumentNullException">Thrown when the URL is null.</exception>
+        /// <exception cref="HttpRequestException">Thrown when the request fails due to an underlying issue.</exception>
         public async Task<HttpResponseMessage> GetAsync(string url)
         {
             return await _client.GetAsync(url);
         }
 
+        /// <summary>
+        /// Wraps the PostAsync method.
+        /// </summary>
+        /// <param name="url">The URL to send the request to.</param>
+        /// <param name="content">The HTTP content (the body, etc)</param>
+        /// <returns>The HTTP response information.</returns>
+        /// <exception cref="System.ArgumentNullException">Thrown when the URL is null.</exception>
+        /// <exception cref="HttpRequestException">Thrown when the request fails due to an underlying issue.</exception>
         public async Task<HttpResponseMessage> PostAsync(string url, HttpContent content)
         {
             return await _client.PostAsync(url, content);
         }
 
+        /// <summary>
+        /// Wraps the PutAsync method.
+        /// </summary>
+        /// <param name="url">The URL to send the request to.</param>
+        /// <param name="content">The HTTP content (the body, etc)</param>
+        /// <returns>The HTTP response information.</returns>
+        /// <exception cref="System.ArgumentNullException">Thrown when the URL is null.</exception>
+        /// <exception cref="HttpRequestException">Thrown when the request fails due to an underlying issue.</exception>
         public async Task<HttpResponseMessage> PutAsync(string url, HttpContent content)
         {
             return await _client.PutAsync(url, content);
         }
 
+        /// <summary>
+        /// Sets the Authorization header with a bearer token.
+        /// </summary>
+        /// <param name="token">The bearer token to use.</param>
         public void SetBearerToken(string token)
         {
             _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
         }
 
+        /// <summary>
+        /// Gets the current bearer token being used by the HTTP client.
+        /// </summary>
+        /// <returns>The current bearer token.</returns>
         public string GetBearerToken()
         {
             if (_client.DefaultRequestHeaders.Authorization == null)
